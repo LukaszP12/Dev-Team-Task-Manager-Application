9272e72dcac5393decb48da61fc2510e3184e976
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-10-29 22:57:02 +0100

    welcome display controller and basic login controller, which will used further for authentication and authorization of users

---
+++ src/main/java/pl/piwowarski/controllers/IntroController.java
@@ -0,0 +1,14 @@
+package pl.piwowarski.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IntroController {
+
+    @GetMapping("/")
+    public String showIndex(){
+        return "index";
+    }
+
+}
