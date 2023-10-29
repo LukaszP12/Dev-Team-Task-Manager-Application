8761001e63af691c8be961419a4b5005c6d94f76
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-10-29 23:03:02 +0100

    controller and views related to displaying of all users

---
+++ src/main/java/pl/piwowarski/controllers/UserController.java
@@ -0,0 +1,28 @@
+package pl.piwowarski.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.piwowarski.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String getAllUsers(Model model, @Param("keyword") String keyword) {
+        model.addAttribute("allUsers", userService.findAllProducts(keyword));
+
+        return "users";
+    }
+
+
+}
