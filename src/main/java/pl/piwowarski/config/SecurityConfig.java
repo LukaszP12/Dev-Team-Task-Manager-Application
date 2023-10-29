3447ebf7fe51fef4a1b863ecc477e7e7e79b001d
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-10-29 23:12:02 +0100

    Setting and configurations related spring security features. Containing access to endpoints for unauthorized users and allowing to privileged ones. Corresponding settings for validation error messages.

---
+++ src/main/java/pl/piwowarski/config/SecurityConfig.java
@@ -0,0 +1,77 @@
+package pl.piwowarski.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.dataSource = dataSource;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Bean(name = "urlService")
+    public boolean geturl() {
+        return true;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .usersByUsernameQuery("select email as principal, password as credentials, true from user where email=?")
+                .authoritiesByUsernameQuery("select u.email as principal, r.role as role from user u inner join user_role ur on(u.user_id=ur.user_id) inner join role r on(ur.role_id=r.role_id) where u.email=?");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/h2-console/**", "/", "/users")
+                .permitAll()
+
+                .antMatchers("/tasks/allTasks")
+                .hasAnyAuthority("COMMON_USER", "ADMIN_USER")
+
+                .antMatchers("/tasks/create", "/tasks/edit/", "/tasks/set-task-completed/")
+                .hasAnyAuthority("ADMIN_USER")
+
+                .antMatchers("/registration/**")
+                .hasAnyAuthority("ADMIN_USER")
+
+                .antMatchers("/assign/**")
+                .hasAnyAuthority("ADMIN_USER")
+
+                .and()
+                .formLogin().loginPage("/login-page").defaultSuccessUrl("/")
+                .loginProcessingUrl("/login-page")
+                .and()
+                .csrf().disable()
+                .logout()
+                .logoutSuccessUrl("/login-page");
+
+        http.csrf().ignoringAntMatchers("/h2-console/**");
+        http.headers().frameOptions().sameOrigin();
+    }
+}
