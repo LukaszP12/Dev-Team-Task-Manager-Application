7584850740434134a516f84bff2d602593f9b9e4
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-08 17:23:55 +0200

    UserDto needed to receiving / sending user representations

---
+++ src/main/java/pl/piwowarski/Dto/UserDto.java
@@ -0,0 +1,28 @@
+package pl.piwowarski.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDto {
+
+    private Long id; // id użytkownika, przy tworzeniu może być null
+
+    @NotBlank(message = "First name cannot be blank")
+    private String firstName;
+
+    @NotBlank(message = "Last name cannot be blank")
+    private String lastName;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email cannot be blank")
+    private String email;
+}
